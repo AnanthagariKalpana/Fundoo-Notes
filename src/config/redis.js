4805d39e8a5bf65b0ef2d7bfa08b3e28813804b0
kalpana <kalpanas1929@gmail.com>
2024-02-29 12:38:25 +0530

    addedRedis

---
+++ src/config/redis.js
@@ -0,0 +1,16 @@
+import { createClient } from "redis";
+import logger from "./logger";
+
+export const client= createClient()
+
+const redis=async()=>{
+    try{
+        await client.connect();
+        logger.info('Connected to Redis Database');
+    }
+    catch(error){
+        logger.error("could not connect to redis",error)
+    }
+};
+
+export default redis;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import helmet from 'helmet';
 
 import routes from './routes';
 import database from './config/database';
+import redis from './config/redis';
+
 import {
   appErrorHandler,
   genericErrorHandler,
@@ -30,6 +32,7 @@ app.use(express.json());
 app.use(morgan('combined', { stream: logStream }));
 app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
 database();
+redis();
 
 
 app.use(`/api/${api_version}`, routes());
